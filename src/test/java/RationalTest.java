e94c0085d5ec5b5eb0585f9a8e8cf57d779ecb9f
Patipat C <patipat.chewprecha@gmail.com>
2023-09-13 21:52:13 +0700

    add test case

--- src/test/java/RationalTest.java
+++ src/test/java/RationalTest.java
@@ -13,4 +13,15 @@ public class RationalTest {
         x.add(y);
         Assert.assertEquals(3, x.numerator);
     }
+    @Test
+    public void testSubtract(){
+        Rational x = new Rational();
+        x.numerator = 1;
+        x.denominator = 2;
+        Rational y = new Rational();
+        y.numerator = 1;
+        y.denominator = 4;
+        x.subtract(y);
+        Assert.assertEquals(1, x.numerator);
+    }
 }
